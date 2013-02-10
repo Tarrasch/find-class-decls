7dde4e980992cb4fa80e568bcb4aea2671fda13d
Arash Rouhani <rarash@gatech.edu>
2013-02-10 14:13:49 -0500

    Huge cleanup

--- ASTPass.cpp
+++ ASTPass.cpp
@@ -14,21 +14,173 @@
 #include <set>
 #include <queue>
 #include <algorithm>
+#include <numeric>
 
-#define all(c) c.begin(), c.end()
 
 using namespace clang;
 using namespace std;
 
+#define all(c) c.begin(), c.end()
+typedef vector < int > VI;
+
+float avg(VI v) { return (accumulate(all(v), 0)+0.0)/v.size(); }
+void minMaxAvg(VI v, string desc) {
+  cout << "Minimum " << desc << ": " << *min_element(all(v)) << endl;
+  cout << "Maximum " << desc << ": " << *max_element(all(v)) << endl;
+  cout << "Average " << desc << ": " << avg(v) << endl;
+  cout << endl;
+}
+
+// Only for task A1
+class MyCallGraph
+: public CallGraph {
+  public:
+    explicit MyCallGraph() {
+      numFunctions = 0;
+    }
+
+    int numFunctions;
+
+    bool VisitFunctionDecl(FunctionDecl  *decl) {
+      numFunctions++;
+      return true;
+    }
+
+    int calcTotalEdges() {
+      int totalEdges = 0;
+      for (const_iterator I = begin(), E = end(); I != E; ++I) {
+        for (CallGraphNode::iterator CI = I->second->begin(),
+            CE = I->second->end(); CI != CE; ++CI) {
+          assert(*CI != getRoot() && "No one can call the root node.");
+        }
+        totalEdges += I->second->size();
+      }
+      return totalEdges;
+    }
+
+    void printStats() {
+      cout << "Number of functions: " << numFunctions << endl;
+      cout << "Total number of edges: " << calcTotalEdges() << endl;
+    }
+
+};
+
+// Only for question A2
+class ASTNodeCounter
+  : public RecursiveASTVisitor<ASTNodeCounter> {
+    public:
+    ASTNodeCounter(){ total = 0; }
+    int total;
+    set < void* > my_set;
+
+    bool VisitType(Type *p){my_set.insert((void*)p);total++; return true;}
+    bool VisitDecl(Decl *p){my_set.insert((void*)p);total++; return true;}
+    bool VisitDeclContext(DeclContext *p){my_set.insert((void*)p);total++; return true;}
+    bool VisitStmt(Stmt *p){my_set.insert((void*)p);total++; return true;}
+
+    int totalNumberOfNodes() { return my_set.size(); }
+    void printStats() {
+      cout << "Ok I found " << totalNumberOfNodes() << " AST nodes in total" << endl;
+    }
+};
+
+// Most important one, A3, A4, A5 and partially A2
 class ASTPassClassVisitor
   : public RecursiveASTVisitor<ASTPassClassVisitor> {
 public:
   explicit ASTPassClassVisitor(ASTContext *context)
-    : context(context) {
-    numFunctions = 0;
+    : context(context) { }
+
+  VI astNodes, blocks, cfgEdges, unreachables;
+
+  bool VisitFunctionDecl(FunctionDecl  *decl) {
+    string fname = decl->getNameInfo().getAsString();
+    cout << fname << endl;
+
+    if(!decl->hasBody()){
+      return true;
+    }
+    // Create CFG
+    Stmt *body = decl->getBody();
+    CFG::BuildOptions bo;
+    CFG *cfg = CFG::buildCFG(decl, body, &decl->getASTContext(), bo);
+
+    // Pretty print CFG
+    /* LangOptions lo; */
+    /* cfg->dump(lo, true); */
+
+    // 3. # Num basic blocks in this function
+    const int num_basicblocks = cfg->getNumBlockIDs();
+    cout << "Total number of basics blocks for function " << fname << ": " << num_basicblocks << endl;
+
+    // 4. # Num basic block edges in this function
+    int tot_edges = 0;
+    for (CFG::const_iterator I = cfg->begin(), E = cfg->end() ; I != E ; ++I) {
+      // Skip the entry block, because we already printed it.
+      const CFGBlock &B(**I);
+
+
+      int num_succs = B.succ_size();
+      cout << "Block " << B.getBlockID() << " has " << num_succs << " successor edges!" << endl;
+      tot_edges += num_succs;
+
+      for (CFGBlock::const_succ_iterator succ_I = B.succ_begin(), succ_E = B.succ_end();
+          succ_I != succ_E; ++succ_I){
+        // Just showing that it's traverseable
+      }
+    }
+    const int tot_cfgEdges = tot_edges;
+
+
+    // 5. # Num unreachable basic blocks
+    // We just do simple bfs by ourselves ...
+    set<CFGBlock*> visited;
+    queue<CFGBlock*> q;
+    q.push(&cfg->getEntry());
+    while(!q.empty()) {
+      CFGBlock *B(q.front()); q.pop();
+      if(!visited.count(B)){
+        visited.insert(B);
+        for (CFGBlock::succ_iterator succ_I = B->succ_begin(), succ_E = B->succ_end();
+            succ_I != succ_E; ++succ_I){
+          q.push(&(**succ_I));
+        }
+      }
+    }
+    int num_reachable = visited.size();
+    const int num_unreachable = num_basicblocks - num_reachable;
+
+    // Now collect data for the end
+    cout << "Number of unreachable blocks " << num_unreachable << endl;
+
+    ASTNodeCounter nc;
+    nc.TraverseDecl(decl);
+
+    astNodes.push_back(nc.totalNumberOfNodes());
+    blocks.push_back(num_basicblocks);
+    cfgEdges.push_back(tot_cfgEdges);
+    unreachables.push_back(num_unreachable);
+    return true;
   }
 
-  int numFunctions;
+  void printStats() {
+    minMaxAvg(astNodes, "number of AST nodes");
+    minMaxAvg(blocks, "number of basic blocks");
+    minMaxAvg(cfgEdges, "total number of cfg-edges");
+    minMaxAvg(unreachables, "total number of unreachable blocks");
+  }
+
+private:
+  ASTContext *context;
+};
+
+// Only for question B
+class PlusReassociator
+  : public RecursiveASTVisitor<PlusReassociator> {
+public:
+  explicit PlusReassociator(ASTContext *context)
+    : context(context) {
+  }
 
   typedef pair<bool, Expr*> P;
   typedef vector<P> VP;
@@ -51,7 +203,6 @@ public:
   void rewriteAddition(BinaryOperator *bo){
     if(bo->getOpcode() == BO_Add){
       cout << "!! BEFORE !!" << endl; bo->dumpAll();
-      cout << "Rewriting started ..." << endl;
       VP vec = linearize(bo);
       stable_sort(all(vec));
 
@@ -70,154 +221,38 @@ public:
 
       cout << "!! AFTER !!" << endl; bo->dumpAll();
       cout << endl << endl;
+
+      // Just to make sure we don't visit the children (for pretty printing
+      // purposes) lets destroy the AST structure
+      bo->setLHS(bo->getRHS());
     }
   }
 
   bool VisitBinaryOperator(BinaryOperator *bo){
-    cout << (bo->getOpcode() == BO_Add ? "Addition" : "Not addition") << endl;
-
     rewriteAddition(bo);
-
     return true;
   }
-
-  bool VisitFunctionDecl(FunctionDecl  *decl) {
-    numFunctions++;
-    string fname = decl->getNameInfo().getAsString();
-    cout << fname << endl;
-
-    if(!decl->hasBody()){
-      cout << "no body :(" << endl;
-    }
-    else {
-      // Create CFG
-      Stmt *body = decl->getBody();
-      CFG::BuildOptions bo;
-      CFG *cfg = CFG::buildCFG(decl, body, &decl->getASTContext(), bo);
-
-      // Pretty print CFG
-      LangOptions lo;
-      cfg->dump(lo, true);
-
-      // 3. # Num basic blocks in this function
-      int num_basicblocks = cfg->getNumBlockIDs();
-      cout << "Total number of basics blocks for function " << fname << ": " << num_basicblocks << endl;
-
-      // 4. # Num basic block edges in this function
-      for (CFG::const_iterator I = cfg->begin(), E = cfg->end() ; I != E ; ++I) {
-        // Skip the entry block, because we already printed it.
-        cout << "SCOOBYDOO" << endl;
-        const CFGBlock &B(**I);
-
-
-        int num_succs = B.succ_size();
-        cout << "Block " << B.getBlockID() << " has " << num_succs << " successor edges!" << endl;
-
-        for (CFGBlock::const_succ_iterator succ_I = B.succ_begin(), succ_E = B.succ_end();
-            succ_I != succ_E; ++succ_I){
-          cout << "\tDoobyScoo" << endl;
-        }
-      }
-
-
-      // 5. # Num unreachable basic blocks
-      // We just do simple bfs on our own
-      set<CFGBlock*> visited;
-      queue<CFGBlock*> q;
-      q.push(&cfg->getEntry());
-      while(!q.empty()) {
-        CFGBlock *B(q.front()); q.pop();
-        if(!visited.count(B)){
-          visited.insert(B);
-          for (CFGBlock::succ_iterator succ_I = B->succ_begin(), succ_E = B->succ_end();
-              succ_I != succ_E; ++succ_I){
-            q.push(&(**succ_I));
-          }
-        }
-      }
-      int num_reachable = visited.size();
-      int num_unreachable = num_basicblocks - num_reachable;
-
-      // Now collect data for the end
-      cout << "Number of unreachable blocks " << num_unreachable << endl;
-    }
-    return true;
-  }
-
-  bool VisitCXXRecordDecl(CXXRecordDecl *Declaration) {
-    if (Declaration->getQualifiedNameAsString() == "n::m::C") {
-      FullSourceLoc FullLocation = context->getFullLoc(Declaration->getLocStart());
-      if (FullLocation.isValid())
-        llvm::outs() << "Found declaration at "
-                     << FullLocation.getSpellingLineNumber() << ":"
-                     << FullLocation.getSpellingColumnNumber() << "\n";
-    }
-    return true;
-  }
-
-  void printStats() {
-    // 1.
-    cout << "Number of functions: " << numFunctions << endl;
-
-    // 2.
-  }
-
 private:
   ASTContext *context;
-};
-
-class MyCallGraph
-  : public CallGraph {
-public:
-  explicit MyCallGraph() {
-    totalEdges = 0;
-  }
-
-  int totalEdges;
-
-  void printStats() {
-    raw_ostream &OS(llvm::errs());
-    for (const_iterator I = begin(), E = end(); I != E; ++I) {
-      if (I->second == getRoot())
-        OS << "< root >";
-      else
-        I->second->print(OS);
-      OS << " calls: ";
-      for (CallGraphNode::iterator CI = I->second->begin(),
-          CE = I->second->end(); CI != CE; ++CI) {
-        assert(*CI != getRoot() && "No one can call the root node.");
-        (*CI)->print(OS);
-        OS << " ";
-      }
-      OS << I->second->size() << '\n';
-      totalEdges += I->second->size();
-    }
-    OS.flush();
-
-    cout << "Total number of edges: " << totalEdges << endl;
-    /* for(const_iterator it = begin();;) { } */
-  }
-
-/*   virtual bool VisitDecl(Decl *decl) { */
-/*     return true; */
-/*   } */
 };
 
 class ASTPassClassConsumer : public clang::ASTConsumer {
   public:
     explicit ASTPassClassConsumer(ASTContext *context)
-      : visitor(context) {}
+      : visitor(context), pr(context) {}
 
     virtual void HandleTranslationUnit(clang::ASTContext &context) {
       visitor.TraverseDecl(context.getTranslationUnitDecl());
       visitor.printStats();
       callgraph.TraverseDecl(context.getTranslationUnitDecl());
-      callgraph.dump();
+      /* callgraph.dump(); */
       callgraph.printStats();
+      pr.TraverseDecl(context.getTranslationUnitDecl());
     }
   private:
     ASTPassClassVisitor visitor;
     MyCallGraph callgraph;
+    PlusReassociator pr;
 };
 
 class ASTPassClassAction : public clang::ASTFrontendAction {
@@ -232,7 +267,7 @@ int main(int argc, char **argv) {
   if (argc > 1) {
     Twine tw(argv[1]);
     clang::tooling::runToolOnCode(new ASTPassClassAction, tw);
-    cout << "\n\n\n\n" << endl;
-    clang::tooling::runToolOnCode(new ASTDumpAction, tw);
+    /* cout << "\n\n\n\n" << endl; */
+    /* clang::tooling::runToolOnCode(new ASTDumpAction, tw); */
   }
 }
